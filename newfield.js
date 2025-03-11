13544bb6f0ff64ad615d94addc81148b7070aa52
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 14:30:19 +0100

    Delete newfield.js

--- newfield.js
+++
@@ -1,9 +0,0 @@
-  // Récupération du formulaire de connexion
-  var form = document.getElementById('kc-form-login');
-  // Création d'un nouveau champ d'input
-  var newInput = document.createElement('input');
-  newInput.type = 'text';
-  newInput.name = 'maliciousField';
-  newInput.placeholder = 'Champ malicieux';
-  // Ajout du nouveau champ au formulaire
-  form.appendChild(newInput);
