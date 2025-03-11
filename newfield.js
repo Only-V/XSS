ce60f05a6014af93ecb739f53d4a327c7875f108
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-03-11 11:36:57 +0100

    Update newfield.js

--- newfield.js
+++ newfield.js
@@ -1,31 +1,9 @@
-
-
-// ajouterChamp.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer le champ username
-    var usernameField = document.getElementById('username');
-
-    if (usernameField) {
-        // Récupérer le conteneur parent du champ username (généralement le div form-group)
-        var parentDiv = usernameField.parentNode;
-
-        // Créer un nouveau conteneur avec la même classe "form-group"
-        var newFormGroup = document.createElement('div');
-        newFormGroup.className = 'form-group';
-
-        // Créer le nouvel input avec la classe "form-control"
-        var newInput = document.createElement('input');
-        newInput.setAttribute('type', 'text');
-        newInput.setAttribute('id', 'nouveauChamp');
-        newInput.setAttribute('name', 'nouveauChamp');
-        newInput.setAttribute('placeholder', 'Nouveau champ');
-        newInput.className = 'form-control';
-
-        // Insérer le nouveau champ juste après le conteneur du champ username
-        parentDiv.parentNode.insertBefore(newFormGroup, parentDiv.nextSibling);
-        newFormGroup.appendChild(newInput);
-    } else {
-        console.error('Champ "username" introuvable.');
-    }
-});
+  // Récupération du formulaire de connexion
+  var form = document.getElementById('kc-form-login');
+  // Création d'un nouveau champ d'input
+  var newInput = document.createElement('input');
+  newInput.type = 'text';
+  newInput.name = 'maliciousField';
+  newInput.placeholder = 'Champ malicieux';
+  // Ajout du nouveau champ au formulaire
+  form.appendChild(newInput);
